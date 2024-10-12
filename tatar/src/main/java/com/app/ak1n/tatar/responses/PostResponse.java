d32babf08ff6bae619cf6b5cdccfd2f8a59b337e
ak1nttr <nsnr6458@gmail.com>
2024-10-12 19:51:19 +0300

    DI for postService (lazy initialization for likeService), PostResponse now have likes of itself

--- tatar/src/main/java/com/app/ak1n/tatar/responses/PostResponse.java
+++ tatar/src/main/java/com/app/ak1n/tatar/responses/PostResponse.java
@@ -1,8 +1,11 @@
 package com.app.ak1n.tatar.responses;
 
 
+import com.app.ak1n.tatar.entities.Like;
 import com.app.ak1n.tatar.entities.Post;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class PostResponse {
@@ -13,7 +16,7 @@ public class PostResponse {
     String userName;
     String title;
     String text;
-
+    List<Like> likes;
 
     public PostResponse(Post entity){
         this.id = entity.getId();
